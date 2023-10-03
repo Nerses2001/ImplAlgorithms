1aa86a23439228490bbb7a849d93543bdb988175
Nerses Hepoyan <nerseshepoyan22@gmail.com>
2023-10-03 05:29:28 -0800

    bubble sort

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using ImplAlgorithms.Sorting;
+
+int[] arr1 = { 5, 3, 1, 4, 2 };
+BoobleSort.Sort(arr1);
+
+for (int i = 0; i < arr1.Length; ++i)
+{
+    Console.Write($"{arr1[i]} ");
+}
---
+++ Sorting/BoobleSort.cs
@@ -0,0 +1,33 @@
+﻿
+namespace ImplAlgorithms.Sorting
+{
+    // Bubble sort algorithm with a complexity of O(n^2)
+    internal class BoobleSort
+    {
+        public static void Sort(int[] input)
+        {
+            for (int i = 0; i < input.Length - 1; ++i)
+            {
+                bool isSorted = true;
+
+                for (int j = 0; j < input.Length - i - 1; ++j)
+                {
+                    if (input[j] > input[j + 1])
+                    {
+                        Swap(ref input[j], ref input[j + 1]);
+                        isSorted = false;
+                    }
+                }
+                if (isSorted)
+                    break;
+                
+            }
+        }
+        private static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
