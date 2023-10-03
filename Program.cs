36c4772a5283e890dd663931401d1995a8cda26a
Nerses Hepoyan <nerseshepoyan22@gmail.com>
2023-10-03 06:21:41 -0800

    Counting  Sort

--- Program.cs
+++ Program.cs
@@ -27,3 +27,12 @@ foreach (int i in arr2)
     Console.Write($"{i}, ");
 }
 
+Console.WriteLine("\n");
+
+int[] arr3 = { 4, 2, 2, 0, 8, 3, 3,-56,-8, 1,-5, 5, 6, 7 ,-5};
+CountingSort.Sort(ref arr3);
+foreach (int i in arr3)
+{
+    Console.Write($"{i}, ");
+
+}
---
+++ Sorting/CountingSort.cs
@@ -0,0 +1,36 @@
+﻿
+namespace ImplAlgorithms.Sorting
+{
+    // CountingSort O(n + k) k = maxElement - minElement
+    internal class CountingSort
+    {
+        public static void Sort(ref int[] input)
+        {
+            if(input.Length < 2) return;
+
+            int max = input.Max();
+            int min = input.Min();
+
+            int[] countsArr = new int[max - min + 1];
+            int[] result = new int[input.Length];
+
+            for(int i = 0; i < input.Length; ++i)
+            {
+                ++countsArr[input[i] - min];
+            }
+
+            int k = 0;
+            for(int i = 0; i < countsArr.Length; ++i)
+            {
+                while (countsArr[i] != 0)
+                {
+                    result[k++] = i + min;
+                    --countsArr[i];
+                }
+            }
+
+            input = result;
+
+        }
+    }
+}
