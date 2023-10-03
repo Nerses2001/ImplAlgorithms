04444c62f4533fa647a34198fd883cc71ea35b83
Nerses Hepoyan <nerseshepoyan22@gmail.com>
2023-10-03 06:07:14 -0800

    Merge Sort'

--- Program.cs
+++ Program.cs
@@ -6,12 +6,24 @@ BubbleSort.Sort(arr1);
 
 for (int i = 0; i < arr1.Length; ++i)
 {
-    Console.Write($"{arr1[i]} ");
+    Console.Write($"{arr1[i]}, ");
 }
 
-Console.WriteLine();
+Console.WriteLine("\n");
+
 List<int> list = PrimNumber.GetPrimNumbers(11);
+
 foreach (int i in list)
 {
-    Console.Write($"{i} ");
+    Console.Write($"{i}, ");
 }
+
+Console.WriteLine("\n");
+
+int[] arr2 = { 12, 11, 13, 5, 6, 7 };
+MergeSort.Sort(arr2);
+foreach (int i in arr2)
+{
+    Console.Write($"{i}, ");
+}
+
---
+++ Sorting/MergeSort.cs
@@ -0,0 +1,45 @@
+﻿
+namespace ImplAlgorithms.Sorting
+{
+    // Complaxity  O(n*log n)
+
+    internal class MergeSort
+    {
+
+        public static void Sort(int[] input)
+        {
+            if(input.Length < 2) return;
+
+            int mid = input.Length / 2;
+
+            int[] left = new int[mid];
+            Array.Copy(input, 0, left, 0, mid);
+            
+            int[] right = new int[input.Length - mid];
+            Array.Copy(input, mid, right, 0, input.Length - mid);
+
+            Sort(left);
+            Sort(right);
+            Marge(input, left,right);
+        }
+
+        private static void Marge(int[] input, int[] left, int[] right)
+        {
+            int leftLength = left.Length;
+            int rightLength = right.Length;
+            int i = 0, j = 0, k = 0;
+
+            while (i < leftLength && j < rightLength)
+            {
+                if (left[i] < right[j]) input[k++] = left[i++];
+                else input[k++] = right[j++];
+            }
+
+            while (i < leftLength)
+                input[k++] = left[i++];
+
+            while (j < rightLength)
+                input[k++] = right[j++];
+        }
+    }
+}
